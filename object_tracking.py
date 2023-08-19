2511a27cfd093144f6d3d7c3315273f8f8442866
TurtleLions <ryanestanislao@gmail.com>
2023-08-19 12:01:43 -0700

    test

--- object_tracking.py
+++ object_tracking.py
@@ -10,10 +10,14 @@ transparent_img = np.zeros((img_height, img_width, n_channels), dtype=np.uint8)
 
 from tkinter import Tk, Canvas, PhotoImage, NW
 
+#244L x 245H
+#0,1194
+
 root = Tk()
 
 root.attributes('-transparentcolor','#000000')
-
+root.attributes('-fullscreen',True)
+root.attributes('-topmost', 'true')
 # Canvas
 canvas = Canvas(root, width=2560, height=1440)
 canvas.pack()
@@ -35,19 +39,21 @@ def update():
     global photoim
     # start time to compute the fps
     start = datetime.datetime.now()
-    screen = np.array(ImageGrab.grab(bbox=(0,0,2560,1440)))
-    screen = screen[:, :, ::-1].copy()
+    #screen = np.array(ImageGrab.grab(bbox=(0,0,2560,1440)))
+    #screen = screen[:, :, ::-1].copy()
+    screenshot = ImageGrab.grab(bbox=(0,1194,244,1439))
+    screenshot.save("ss.png")
     #cv2.imshow('Python Window', screen)
     transparent_img = np.zeros((img_height, img_width, n_channels), dtype=np.uint8)
     # run the YOLO model on the frame
-    detections = model.predict(screen)
+    detections = model.predict("ss.png")
     result = detections[0]
     # loop over the detections
     for box in result.boxes:
         x1, y1, x2, y2 = [
           round(x) for x in box.xyxy[0].tolist()
         ]
-        cv2.rectangle(transparent_img, (x1, y1) , (x2, y2), GREEN, 2)
+        cv2.rectangle(transparent_img, (x1, y1+1194) , (x2, y2+1194), GREEN, 2)
 
     # end time to compute the fps
     end = datetime.datetime.now()
@@ -57,13 +63,14 @@ def update():
 
     # calculate the frame per second and draw it on the frame
     fps = f"FPS: {1 / total:.2f}"
-    cv2.putText(screen, fps, (50, 50),
-                cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 8)
+    #cv2.putText(screen, fps, (50, 50),
+    #            cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 8)
     cv2.imwrite("hi.jpg", transparent_img)
     # show the frame to our screen
     photoim = ImageTk.PhotoImage(Image.open("hi.jpg"))
     canvas.create_image(0, 0, anchor=NW, image=photoim)
     canvas.update()
+    root.attributes('-topmost', 'true')
     root.after(1000,update)
 #cv2.destroyAllWindows()
 root.after(1000,update)
